7b2a7b441fab504697c9170827cb9acd35954626
Sorades <2019302080119@whu.edu.cn>
2021-03-12 07:58:36 +0800

    第三次作业

---
+++ hw03/T1/ShapeDemo.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    class ShapeDemo
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Rectangle rec = new Rectangle(2, 3);
+                Console.WriteLine($"长方形的面积：{rec.Area()}");
+                Console.WriteLine($"长方形的周长：{rec.Primeter()}\n");
+
+                Square sq = new Square(6);
+                Console.WriteLine($"正方形的面积：{sq.Area()}");
+                Console.WriteLine($"正方形的周长：{sq.Primeter()}\n");
+
+                Triangle tr = new Triangle(1, 1, 1);
+                Console.WriteLine($"三角形的面积：{tr.Area()}");
+                Console.WriteLine($"三角形的周长：{tr.Primeter()}\n");
+            }
+            catch (ShapeIllegalException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    public interface Shape
+    {
+        /// <returns>图形面积</returns>
+        double Area();
+        /// <returns>图形周长</returns>
+        double Primeter();
+        /// <returns>图形数据是否合法</returns>
+        bool IsLegal();
+        string ShapeInfo();
+    }
+
+    public class Rectangle : Shape
+    {
+        public double width { get; }
+        public double height { get; }
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public double Area()
+        {
+            if (!IsLegal())
+                throw new ShapeIllegalException($"{this.GetType().Name}图形数据不合法！");
+            return width * height;
+        }
+
+        public bool IsLegal()
+        {
+            return width > 0 && height > 0 ? true : false;
+        }
+
+        public double Primeter()
+        {
+            if (!IsLegal())
+                throw new ShapeIllegalException($"{this.GetType().Name}图形数据不合法！");
+            return 2 * (width + height);
+        }
+
+        string Shape.ShapeInfo()
+        {
+            return $"高：{this.height.ToString("0.000")}，宽：{this.width.ToString("0.000")}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public Square(double side) : base(side, side) { }
+    }
+
+    public class Triangle : Shape
+    {
+        public double sideA { get; }
+        public double sideB { get; }
+        public double sideC { get; }
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+        public double Area()
+        {
+            if (!IsLegal())
+                throw new ShapeIllegalException($"{this.GetType().Name}图形数据不合法！");
+            double p = (sideA + sideB + sideC) / 2;
+            return Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+        }
+
+        public bool IsLegal()
+        {
+            if (sideA + sideB <= sideC)
+                return false;
+            if (sideA + sideC <= sideB)
+                return false;
+            if (sideB + sideC <= sideA)
+                return false;
+            return true;
+        }
+
+        public double Primeter()
+        {
+            if (!IsLegal())
+                throw new ShapeIllegalException($"{this.GetType().Name}图形数据不合法！");
+            return sideA + sideB + sideC;
+        }
+
+        string Shape.ShapeInfo()
+        {
+            return $"三边分别为：{sideA.ToString("0.000")}, {sideB.ToString("0.000")}, {sideC.ToString("0.000")}";
+        }
+    }
+    class ShapeIllegalException : Exception
+    {
+        /// <summary>
+        /// 抛出数据不合法的异常
+        /// </summary>
+        /// <param name="message"></param>
+        public ShapeIllegalException(string message) : base(message) { }
+    }
+}
---
+++ hw03/T2/FactoryDemo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T1;
+
+namespace T2
+{
+    class FactoryDemo
+    {
+        static void Main(string[] args)
+        {
+            string[] shapeName = { "Rectangle", "Square", "Triagnle" };
+            Random ra = new Random();
+            ShapeFactory shapeFactory = new ShapeFactory();
+            double areaSum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Shape shape = shapeFactory.GetShapeRandomly(shapeName[ra.Next(0, 3)]);
+                Console.WriteLine($"创建{shape.GetType().Name},\t{shape.ShapeInfo()}");
+                areaSum += shape.Area();
+            }
+            Console.WriteLine($"\n这些图形的总面积为：{areaSum.ToString("0.000")}");
+        }
+    }
+    class ShapeFactory
+    {
+        /// <summary>
+        /// 简单工厂模式实现
+        /// </summary>
+        /// <param name="shapeType"></param>
+        /// <returns></returns>
+        public Shape GetShapeRandomly(string shapeType)
+        {
+            Random ra = new Random();
+            if (shapeType == null)
+                return null;
+            else if (shapeType == "Rectangle")
+                return new Rectangle(ra.NextDouble() * 10, ra.NextDouble() * 10);
+            else if (shapeType == "Square")
+                return new Square(ra.NextDouble() * 10);
+            else if (shapeType == "Triagnle")
+            {
+                double sideTemp = ra.NextDouble();
+                return new Triangle(sideTemp * 3, sideTemp * 4, sideTemp * 5);
+            }
+            return null;
+        }
+    }
+
+}
+
+

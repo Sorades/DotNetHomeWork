21a5c917ad9bc2210b335f0a737a0b3219787220
Sorades <2019302080119@whu.edu.cn>
2021-03-17 22:13:37 +0800

    第四次作业

---
+++ hw04/T1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            int[] arr = { 1, 2, 3, 6 };
+            foreach (int i in arr)
+                list.Add(i);
+            //遍历打印：
+            Console.WriteLine("遍历打印：");
+            list.ForEach(m => Console.WriteLine("\t " + m));
+            //求最大值：
+            int max = list.Head.Data;
+            list.ForEach(m => max = max < m ? m : max);
+            Console.WriteLine($"最大值:{max}");
+            //求最小值：
+            int min = list.Head.Data;
+            list.ForEach(m => min = min > m ? m : min);
+            Console.WriteLine($"最小值:{min}");
+            //求和：
+            int sum = 0;
+            list.ForEach(m => sum += m);
+            Console.WriteLine($"求和:{sum}");
+
+        }
+    }
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        /// <summary>
+        /// 初始化链表
+        /// </summary>
+        public GenericList()
+        {
+            head = tail = null;
+        }
+        public Node<T> Head { get => head; }
+        /// <summary>
+        /// 添加节点
+        /// </summary>
+        /// <param name="t"></param>
+        public void Add(T t)
+        {
+            Node<T> node = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                tail.Next = node;
+                tail = node;
+            }
+        }
+        /// <summary>
+        /// 从头遍历链表
+        /// </summary>
+        /// <param name="action"></param>
+        public void ForEach(Action<T> action)
+        {
+            Node<T> temp = head;
+            while (temp != null)
+            {
+                action(temp.Data);
+                temp = temp.Next;
+            }
+        }
+    }
+}
---
+++ hw04/T2/ClockDemo.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2
+{
+    public delegate void ClockHandler(Time time);
+
+    class ClockDemo
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            try
+            {
+                clock.SetTime(1, 59, 59);
+                clock.SetAlarm(2, 0, 5);//设置五秒后闹铃
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            clock.SwitchAlarm(true);//打开闹钟开关
+            clock.Alarm += new ClockHandler(Alarm);
+            clock.Tick += new ClockHandler(Tick);
+            clock.TimeGoesOn(10);//钟走10s
+
+
+        }
+        /// <summary>
+        /// 每秒报Tick和时间
+        /// </summary>
+        /// <param name="time"></param>
+        static void Tick(Time time)
+        {
+            Console.WriteLine($"time:  {time.Hour.ToString("00")}:{time.Minute.ToString("00")}:{time.Second.ToString("00")}\nTick...");
+        }
+        /// <summary>
+        /// 触发闹钟
+        /// </summary>
+        /// <param name="alarm"></param>
+        static void Alarm(Time alarm)
+        {
+            Console.WriteLine("!!!!!Alarm!!!!!\n");
+        }
+    }
+    /// <summary>
+    /// 发布器
+    /// </summary>
+    public class Clock
+    {
+        public event ClockHandler Tick;
+        public event ClockHandler Alarm;
+        Time time;
+        Time alarm;
+        bool IsSetAlarm = false;
+        public Clock()
+        {
+            time = new Time()
+            {
+                Hour = 0,
+                Minute = 0,
+                Second = 0
+            };
+            alarm = new Time();
+        }
+        /// <summary>
+        /// 闹钟走时
+        /// </summary>
+        /// <param name="timeLength">走时的时间长度</param>
+        public void TimeGoesOn(int timeLength)
+        {
+            for (int i = 0; i < timeLength; i++)
+            {
+                time.Second++;
+                if (time.Second == 60)
+                {
+                    time.Second = 0;
+                    time.Minute++;
+                }
+                if (time.Minute == 60)
+                {
+                    time.Minute = 0;
+                    time.Hour++;
+                }
+                if (time.Hour == 24)
+                    time.Hour = 0;
+                Tick(time);
+                if (IsSetAlarm && time.Equals(alarm))
+                    this.Alarm(alarm);
+            }
+        }
+        /// <summary>
+        /// 设置钟的时间
+        /// </summary>
+        /// <param name="hour">时</param>
+        /// <param name="minute">分</param>
+        /// <param name="second">秒</param>
+        public void SetTime(int hour, int minute, int second)
+        {
+            if (hour >= 24 || hour < 0 ||
+                minute >= 60 || minute < 0 ||
+                second >= 60 || second < 0)
+                throw new ArgumentException("时间格式不对");
+            time.Hour = hour;
+            time.Minute = minute;
+            time.Second = second;
+        }
+        /// <summary>
+        /// 设置闹钟的时间
+        /// </summary>
+        /// <param name="hour">时</param>
+        /// <param name="minute">分</param>
+        /// <param name="second">秒</param>
+        public void SetAlarm(int hour, int minute, int second)
+        {
+            if (hour >= 24 || hour < 0 ||
+                minute >= 60 || minute < 0 ||
+                second >= 60 || second < 0)
+                throw new ArgumentException("时间格式不对");
+            alarm.Hour = hour;
+            alarm.Minute = minute;
+            alarm.Second = second;
+        }
+        public void SwitchAlarm(bool flag) { IsSetAlarm = flag; }
+
+    }
+    /// <summary>
+    /// 时间类
+    /// </summary>
+    public class Time
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+        /// <summary>
+        /// 重写父类Equals，判断两个对象的值是否相等
+        /// </summary>
+        /// <param name="obj">比较的对象</param>
+        /// <returns>值是否相等</returns>
+        public override bool Equals(object obj)
+        {
+            Time t = obj as Time;
+            if ((this.Hour == t.Hour) &&
+                (this.Minute == t.Minute) &&
+                (this.Second == t.Second))
+            {
+                return true;
+            }
+            else { return false; }
+        }
+    }
+}
